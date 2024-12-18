c6fb54ada125f191a7de1093ddacb86015dd4938
Nick Schrock <schrockn@dagsterlabs.com>
2024-12-18 10:47:56 -0500

    cp

--- python_modules/libraries/dagster-components/dagster_components_tests/unit_tests/test_custom_component.py
+++ python_modules/libraries/dagster-components/dagster_components_tests/unit_tests/test_custom_component.py
@@ -1,19 +1,7 @@
-import inspect
-import textwrap
-
-from dagster_components.lib.custom_component import (
-    custom_component_template,
-    get_custom_component_template,
-)
+from dagster_components.lib.custom_component import get_custom_component_template
 
 
 def test_custom_component_template() -> None:
-    raw_source = inspect.getsource(custom_component_template)
-    without_decl = raw_source.split("\n", 1)[1]
-    dedented = textwrap.dedent(without_decl)
+    source_from_fn = get_custom_component_template("Foo", "foo")
 
-    source = textwrap.dedent(raw_source).split("\n", 1)[1]
-    source_from_fn = get_custom_component_template()
-    import code
-
-    code.interact(local=locals())
+    assert "class FooComponent" in source_from_fn
